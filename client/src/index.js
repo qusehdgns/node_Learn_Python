e94c969cd6003a2c27d904d33b9548a73ef79c00
qusehdgns <qusehdgns123@gmail.com>
2021-06-01 23:30:05 +0900

    redux change final end

--- client/src/index.js
+++ client/src/index.js
@@ -19,8 +19,7 @@ import Reducer from './_reducers';
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 
-ReactDOM.render(<Provider store={createStoreWithMiddleware(Reducer,
-                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+ReactDOM.render(<Provider store={createStoreWithMiddleware(Reducer)}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
